8d45b579c6e98680c404e44dd0722de11a55c8fb
Matt Andersen <matthewnandersen@gmail.com>
2019-05-15 12:47:49 -0400

    Revert changes made to RotationTransformation function

--- SimulateLambda.cpp
+++ SimulateLambda.cpp
@@ -5,10 +5,6 @@
 #define PRMASS 938.27231
 
 //GLOBAL DECLARATIONS
-TFile *hOutput = new TFile("simulationHistos.root", "recreate");
-TH1F *hV0M   = new TH1F("hV0M", "Invariant Mass;Mass [MeV/c2];Counts", 500, 1000, 1600);
-//TH1F *hTheta   = new TH1F("hTheta", "Theta Dist.; Theta; Counts", 500, 0, 3.15);
-
 double trueRight = 0;
 double trueLeft  = 0;
 
@@ -40,7 +36,7 @@ TLorentzVector CreatePion(TRandom3 random) {
     double P = TMath::Sqrt((V0MASS*V0MASS - (PIMASS + PRMASS)*(PIMASS + PRMASS))*
                            (V0MASS*V0MASS - (PIMASS - PRMASS)*(PIMASS - PRMASS)))/(2*V0MASS);
     double Phi = random.Uniform(0, 2*3.14159);
-    double Theta = 0;//TMath::ACos(ICos.GetRandom());
+    double Theta = (TMath::Pi() - TMath::ACos(ICos.GetRandom()));
     double E = TMath::Sqrt((P)*(P) + (PIMASS)*(PIMASS));
     
     double Pz = P*cos(Theta);
@@ -68,23 +64,15 @@ TLorentzVector RotationTransform(TLorentzVector Lambda, TLorentzVector Daughter)
     double V0Theta = Lambda.Theta();
     double V0Phi = Lambda.Phi();
     
-    TVector3 Lambda3 = Lambda.Vect();
-    Lambda3.RotateZ(-V0Phi);
-    Lambda3.RotateY(-V0Theta);
-    
-    /*TRotation rotMatrix;
+    TRotation rotMatrix;
     TRotation r1;
     TRotation r2;
-    r1.RotateZ(-V0Phi);
-    r2.RotateY(-V0Theta);
-    rotMatrix = r1 * r2;*/
-
-    //cout << Lambda3.X() << ", " << Lambda3.Y() << ", " << Lambda3.Z() << endl;
+    r2.RotateZ(V0Phi);
+    r1.RotateY(V0Theta);
+    rotMatrix = r2 * r1;
     
-    TVector3 rotatedDaughter3 = Daughter.Vect();
-    rotatedDaughter3.RotateZ(-V0Phi);
-    rotatedDaughter3.RotateY(-V0Theta);
-    //rotatedDaughter3 = rotMatrix * (Daughter.Vect());
+    TVector3 rotatedDaughter3;
+    rotatedDaughter3 = rotMatrix * (Daughter.Vect());
     TLorentzVector rotatedDaughter (rotatedDaughter3, Daughter.E());
     return rotatedDaughter;
 }
@@ -114,12 +102,16 @@ void SimulateMeasurement(int index, string includeCuts, int momentumCuts, TLoren
 }
 
 //Simple Helicity calculator
-double GetHelicity(TLorentzVector LambdaInLab, TLorentzVector ProtonInLambda) {
-    double Helicity = LambdaInLab.Vect()*ProtonInLambda.Vect();
+double GetHelicity(TLorentzVector Lambda, TLorentzVector Proton) {
+    double Helicity = Lambda.Vect()*Proton.Vect();
     return Helicity;
 }
 
 void SimulateLambda() {
+    //Initialize histograms
+    TFile *hOutput = new TFile("simulationHistos.root", "recreate");
+    TH1F *hV0M   = new TH1F("hV0M", "Invariant Mass;Mass [MeV/c2];Counts", 500, 1000, 1600);
+    
     string includeRes;
     cout << "Include Resolution (y/n)? ";
     cin >> includeRes;
@@ -134,17 +126,13 @@ void SimulateLambda() {
     }
     
     //Simulate Events
-    for (int event=0; event<1000; event++) {
+    for (int event=0; event<10000; event++) {
         //Generate 10 Lambdas/Pions/Protons for each event
         for (int V0Num=0; V0Num<10; V0Num++) {
             r.SetSeed();
             TLorentzVector LambdaInLab = CreateLambda(r);
             TLorentzVector PionInLambda = CreatePion(r);
             TLorentzVector ProtonInLambda = CreateProton(PionInLambda);
-            //cout << PionInLambda.Theta() << endl;
-            //cout << ProtonInLambda.Theta() << endl;
-            
-            //hTheta->Fill(ProtonInLambda.Theta());
             
             TLorentzVector PionInRot = RotationTransform(LambdaInLab, PionInLambda);
             TLorentzVector ProtonInRot = RotationTransform(LambdaInLab, ProtonInLambda);
@@ -153,7 +141,7 @@ void SimulateLambda() {
             TLorentzVector ProtonInLab = LorentzTransform(LambdaInLab, ProtonInRot);
             
             //Determine True Helicity
-            double trueHelicity = GetHelicity(LambdaInLab, ProtonInLambda);
+            double trueHelicity = GetHelicity(LambdaInLab, ProtonInRot);
             if (trueHelicity > 0) trueRight++;
             if (trueHelicity < 0) trueLeft++;
             
@@ -186,10 +174,9 @@ void SimulateLambda() {
                 hV0M->Fill(V0M);
                 
             }//End pion track loop
-         }//End proton track loop
+        }//End proton track loop
     }//End Event Loop
     
-    //hTheta->Write();
     hV0M->Write();
     hOutput->Close();
     
